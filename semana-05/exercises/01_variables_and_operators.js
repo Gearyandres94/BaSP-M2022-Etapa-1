eb166d258bbc33e0f9d241af1c052106d2b5095a
Andres Geary <gearyandres@gmail.com>
2022-04-16 12:19:16 -0300

    BaSP-W05-2022: ex02-Strings

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -1,16 +1,16 @@
-/*The excersisez are the next:
-1)sum two int variables and store the result in a third variable
-2)Concatenate two string variables and store it in a third variable.
-3)Create two string variables and store the sum of the lenght of both strings in a third variable.
+/*The excersises are the next:
+a)sum two int variables and store the result in a third variable
+b)Concatenate two string variables and store it in a third variable.
+c)Create two string variables and store the sum of the lenght of both strings in a third variable.
 */
 
-//Excersise 1
+//Excersise a
 
 var num1=5, num2=6, res=num1+num2;
 
 console.log(res);
 
-//excersise 2
+//excersise b
 
 var firstName='Andres';
 
@@ -20,10 +20,11 @@ var fullName= firstName + lastName;
 
 console.log(fullName);
 
-// excersize 3
-var firstCountry=Argentina;
+// excersize c
 
-var secondCountry=Brasil;
+var firstCountry='Argentina';
+
+var secondCountry='Brasil';
 
 var nameLenght = firstCountry.length + secondCountry.length;
 
---
+++ semana-05/exercises/02_strings.js
@@ -0,0 +1,70 @@
+/*The excersises are the next:
+a)Create a variable of type string with at least 10 char and convert the text to uppercase. 
+b)Create a variable of type string with at least 10 char and generate a new string with the first 5 char, 
+saving it in a new variable.
+c)Create a variable of type string with at least 10 char and generate a new string with the last 3 char, 
+saving it in a new variable.
+d)Create a variable of type string with at least 10 char and generate a new string with the first letter uppercase
+and the rest in lowercase. Save the result in a new variable
+e)Create a variable of type string with at least 10 char and a whitespace. Find the position of the whitespace
+and store it in a new variable.
+f)Create a variable of type string with at least two long words (ten char and some white space between them)
+and generate a new string that contains the first letter in uppercase and the rest in lowercase.
+*/
+
+//Excersise a
+
+var stringBefore='alfa_beta_epsilon';
+
+var stringAfter= stringBefore.toUpperCase();
+
+console.log(stringAfter);
+
+//Excersise b
+
+var chain = 'aabbccddee';
+
+var subChain= chain.substring(0,5);
+
+console.log(subChain);
+
+//Excersise c
+
+subChain= chain.substring(7,10);
+
+console.log(subChain);
+
+//Excersise d
+
+subChain= chain.substring(0,1);
+
+subChain= subChain.toUpperCase();
+
+subChain= subChain + chain.substring(1,10);
+
+console.log(subChain);
+
+//Excersise e
+
+var name = 'Andres Geary';
+
+var whiteSpace = name.indexOf(' ',0);
+
+console.log('The blank space is located in the position ',whiteSpace);
+
+//Excersise f
+
+var countries = 'argentina e inglaterra';
+
+var countriesUpperCase = countries.substring(0,1).toUpperCase(); 
+
+countriesUpperCase = countriesUpperCase + countries.substring(1,countries.indexOf(' ',0));
+
+countriesUpperCase = countriesUpperCase + countries.substring(countriesUpperCase.length, countriesUpperCase.length+3);
+
+countriesUpperCase = countriesUpperCase + 
+countries.substring(countriesUpperCase.length,countriesUpperCase.length+1).toUpperCase();
+
+countriesUpperCase= countriesUpperCase + countries.substring(countriesUpperCase.length,countries.length);
+
+console.log(countriesUpperCase);
